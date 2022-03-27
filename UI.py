5a0ebc489b5a289657df22ee4bc54eaf0af836ae
James Li <1013436135@qq.com>
2022-03-27 20:13:35 +0800

    @lmc: changed some structure.

--- UI.py
+++ UI.py
@@ -26,23 +26,18 @@ import self_play
 import shared_storage
 import trainer
 
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 500
+
 def window_1(games):
-    # root = Tk()  # 创建窗口对象的背景色
     global root
     root = Tk()
     root.title("muzero")
-    screenwidth = root.winfo_screenwidth()  # 屏幕宽度
-    screenheight = root.winfo_screenheight()  # 屏幕高度
-    width = 600
-    height = 500
-    x = int((screenwidth - width) / 2)
-    y = int((screenheight - height) / 2)
-    label = Label(root, text=" ",height=5, font='Arial 20 bold')
-    label.grid(column=3, row=4)
-    root.geometry('{}x{}+{}+{}'.format(width, height, x, y))  # 大小以及位置
-    L1 = Label(root, text="Welcome to MuZero! Here's a list of games:", height=5, font='Arial 20 bold')
+    screenwidth = root.winfo_screenwidth()
+    screenheight = root.winfo_screenheight()
+    root.geometry('{}x{}+{}+{}'.format(WINDOW_WIDTH, WINDOW_HEIGHT, 0, 0))
+    L1 = Label(root, text="Welcome to MuZero!\nHere's a list of games:", height=5, font='Arial 20 bold')
     L1.grid(column=2, row=1)
-    #L1.pack()
     global combobox
     combobox = ttk.Combobox(
         master=root,  # 父容器
@@ -55,11 +50,9 @@ def window_1(games):
     )
 
     combobox.grid(column=2, row=2)
-    #combobox.pack()
 
     B = Button(root, text="Next", width=10, pady=20, command=chooseGame)
     B.grid(column=2, row=5)
-    #B.pack(side="bottom")
     root.mainloop()
 
 
@@ -67,11 +60,12 @@ def chooseGame():
     global game_name
     global muzero
     game_name = combobox.get()
-    muzero = MuZero(game_name)
+    #muzero = MuZero(game_name)
     root.destroy()
+    window_2(game_name)
 
-def window_2(options):
-    # root = Tk()  # 创建窗口对象的背景色
+def window_2(game):
+    print(game)
     global root1
     root1 = Tk()
     root1.title("muzero")
@@ -88,6 +82,17 @@ def window_2(options):
     L1.grid(column=2, row=1)
     #L1.pack()
     global combobox1
+    # Configure running options
+    options = [
+        "Train",
+        "Load pretrained model",
+        "Diagnose model",
+        "Render some self play games",
+        "Play against MuZero",
+        "Test the game manually",
+        "Hyperparameter search",
+        "Exit",
+    ]
     combobox1 = ttk.Combobox(
         master=root1,  # 父容器
         height=10,  # 高度,下拉显示的条目数量
@@ -103,7 +108,6 @@ def window_2(options):
 
     B1 = Button(root1, text="Next", width=10, pady=20, command=chooseGame1)
     B1.grid(column=2, row=5)
-    #B.pack(side="bottom")
     root.mainloop()
 
 
@@ -128,9 +132,10 @@ if __name__ == "__main__":
             for filename in sorted(list((pathlib.Path.cwd() / "games").glob("*.py")))
             if filename.name != "abstract_game.py"
         ]
-        window_1(games);
+        print(games)
+        window_1(games)
 
-        while True:
+        while False:
             # Configure running options
             options = [
                 "Train",
@@ -142,7 +147,7 @@ if __name__ == "__main__":
                 "Hyperparameter search",
                 "Exit",
             ]
-            window_2(options);
+            window_2(options)
             choice = int(choice)
             if choice == 0:
                 muzero.train()
